4d1ce8715401f42901a0c6d673dacebeb924d28d
Misterdev19 <jcpenagos716@outlook.com>
2023-03-18 17:09:00 -0500

    datatables

--- js/index.js
+++ js/index.js
@@ -1,10 +1,49 @@
-const init = ()=> {
+const init = () => {
 
-    $("#btn_detalle").on("click" , ()=>{ 
+    $("#btn_detalle").on("click", () => {
         $("#modal_detalle").modal("show");
     });
-     
+
+    get_list_user();
+
 }
 
+const get_list_user = () => {
+
+
+    $("#table_usuarios")
+        .dataTable({
+            aProcessing: true,
+            aServerSide: true,
+            "processing": true,
+            language: {
+                url: "",
+                processing: `<div class="loader"></div>`
+            },
+            dom: "Bfrtip",
+            responsive: true,
+            lengthChange: false,
+            autoWidth: false,
+            buttons: ["copy", "csv", "excel", "pdf", "print", "colvis"],
+            ajax: {
+                url: "",
+                // xhr: function() {
+
+                //    console.log("se cancelo");
+
+                // },
+                type: "post",
+                data: {},
+                dataType: "json",
+                error: function (e) {
+                    // console.log(e.responseText);
+                },
+            },
+            bDestroy: true,
+            iDisplayLength: 10, //paginacion
+            order: [[0, "desc"]], //ordenar (columna , orden)
+        })
+        .dataTable();
+}
 
 init();
